ffcf6615f71d7c842e605bb96d639cfdca773b32
Tanmoy Chanda <tanmoy.chanda@outlook.com>
2024-04-15 15:52:55 +0530

    update

--- script.js
+++ script.js
@@ -249,3 +249,14 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+
+const scrollProgress = document.getElementById('scroll-progress');
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
